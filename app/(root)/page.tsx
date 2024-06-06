c1c43f4a2fc1422897c67a2a75e4d3b0cb83d3a3
navneet9971 <Navneetjha2012@gmail.com>
2024-06-06 18:06:45 +0530

    Done card layouts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
+import PodcastCard from "@/components/PodcastCard"
 import { Button } from "@/components/ui/button"
+import { podcastData } from "@/constants"
 
 
 
@@ -9,7 +11,18 @@ const Home = () => {
         <h1 className='text-20 font-bold text-white-1'>
           Trending Podcasts
         </h1>
-
+        <div className="podcast_grid">
+        {podcastData.map(({ id, title, description, imgURL }) => (
+    <PodcastCard
+    key={id}
+    imgUrl = {imgURL}
+    title = {title}
+    description = {description}
+    podcastId={id}
+    />
+  ))}
+        </div>
+  
       </section>
       </div>
   )
--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,8 +1,17 @@
+'use client';
+
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import { usePathname, useRouter } from 'next/navigation'
+
 
 const LeftSidebar = () => {
+
+    const pathname = usePathname();
+const router = useRouter();
+
   return (
     <section className='left_sidebar'>
         <nav className='flex flex-col gap-6'>
@@ -12,6 +21,17 @@ const LeftSidebar = () => {
            <h1 className='text-24 font-extrabold text-white max:lg:hidden'>VodCaster</h1>
             </Link>
      
+     {sidebarLinks.map(({ route, label, imgURL }) => {
+
+const isActive = pathname === route || pathname.startsWith(`${route}/`)
+
+        return <Link href={route} key={label}
+        className={cn('flex gap-3 items-center py-4 max-lg:px-4 justify-start', {'bg-nav-focus border-r-4 border-orange-1': isActive})} >
+            <Image src={imgURL} alt='label' width={24} height={24}/>
+            <p>{label}</p>
+        </Link>
+     })
+     }
         </nav>
     </section>
   )
